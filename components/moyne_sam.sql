d29f7e4ef5795fefd01cf38c401d03e9fe1c4e7f
usases <igor.sergeev.05@list.ru>
2025-04-18 01:49:42 +0300

    123

--- components/moyne_sam.sql
+++ components/moyne_sam.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Апр 17 2025 г., 23:29
+-- Время создания: Апр 18 2025 г., 00:49
 -- Версия сервера: 10.4.32-MariaDB
 -- Версия PHP: 8.2.12
 
@@ -46,8 +46,7 @@ CREATE TABLE `service_requests` (
 --
 
 INSERT INTO `service_requests` (`id`, `user_id`, `service_type_id`, `address`, `contact_info`, `date_time`, `payment_type`, `status`, `cancellation_reason`, `created_at`, `updated_at`) VALUES
-(5, 1, 2, '123', '123', '2025-04-24 04:06:00', 'online', 'cancelled', '213', '2025-04-17 21:02:11', '2025-04-17 21:02:22'),
-(6, 1, 2, '123', '123', '2025-04-25 04:16:00', 'cash', 'cancelled', '212', '2025-04-17 21:11:41', '2025-04-17 21:13:28');
+(9, 4, 4, '123', '123', '2025-04-18 03:48:00', 'cash', 'new', NULL, '2025-04-17 22:46:58', '2025-04-17 22:46:58');
 
 -- --------------------------------------------------------
 
@@ -67,7 +66,7 @@ CREATE TABLE `service_types` (
 --
 
 INSERT INTO `service_types` (`id`, `name`, `description`, `price`) VALUES
-(2, '123', '123', 10000.00);
+(4, 'Уборка', 'Уборка', 1000.00);
 
 -- --------------------------------------------------------
 
@@ -91,8 +90,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `full_name`, `phone`, `email`, `login`, `password`, `created_at`, `role`) VALUES
-(1, 'асуждаю', '+7 (999) 999 99 99', 'dsffds@mail.ru', 'usases', '$2y$10$VipRIKsMqE4I/mwlJqhRYeun6Dy7UXMQ80iXQQvUToc8Oa7BsClcW', '2025-04-17 18:42:35', 'user'),
-(2, 'Ппапаа Упапа Пап', '+7 (923) 232 32 32', 'ge@mail.ru', 'okad', '$2y$10$HYY/23/E0SlNw4NMDibrauLHSG.5l/xeR5k4UsSvY0YrCU10UOZ5W', '2025-04-17 20:35:47', 'user');
+(4, 'Ыавва Аваав Яавва', '+7 (999) 999 99 99', 'usases@mail.ru', 'usases', '$2y$10$gxWHcmxVXys60xBnmavZR.JDY8ZgAGkxdXH.rsC3t3KbVjbbGNCVS', '2025-04-17 22:45:32', 'admin');
 
 --
 -- Индексы сохранённых таблиц
@@ -128,19 +126,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `service_requests`
 --
 ALTER TABLE `service_requests`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT для таблицы `service_types`
 --
 ALTER TABLE `service_types`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
